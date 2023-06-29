bd2d8fd99b8ec2b6dbe54c9177b45ff1e6185a21
svelterust <svelterust@svelterust.com>
2023-06-29 15:40:13 +0200

    Serialization

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,4 +1,21 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct User {
+    name: String,
+    age: u32,
+    money: f32,
+}
+
 #[no_mangle]
-fn main() {
-    println!("Hello, world!");
+fn main(input_ptr: u32) {
+    // Read object from memory
+    let input = unsafe {
+        let len = *(input_ptr as *const u32);
+        let bytes = (input_ptr + 4) as *const u8;
+        let slice = core::slice::from_raw_parts(bytes, len as usize);
+        rmp_serde::from_slice::<User>(slice)
+    };
+    
+    println!("{input:?}");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use serde_json::{json, Value as Json};
 use wasmer::{Instance, Module, Store, Value};
 use wasmer_wasi::WasiState;
 
@@ -25,9 +26,25 @@ impl Engine {
         Ok(Self { store, instance })
     }
 
-    fn run(&mut self, params: &[Value]) -> Result<Box<[Value]>> {
+    fn run(&mut self, input: Json) -> Result<Box<[Value]>> {
         let function = self.instance.exports.get_function("main")?;
-        let result = function.call(&mut self.store, params)?;
+        let heap_start = 0x110000;
+
+        // Serialize to bytes
+        let serialized = rmp_serde::to_vec(&input)?;
+        let input_len = (serialized.len() as u32).to_le_bytes();
+        let input_bytes = [&input_len[..], &serialized].concat();
+
+        // Grow memory for input
+        let memory = self.instance.exports.get_memory("memory")?;
+        let pages = (input_bytes.len() / wasmer::WASM_PAGE_SIZE) + 1;
+        memory.grow(&mut self.store, pages as u32)?;
+
+        // Write bytes into memory
+        let view = memory.view(&self.store);
+        view.write(heap_start as u64, &input_bytes)?;
+
+        let result = function.call(&mut self.store, &[Value::I32(heap_start)])?;
         Ok(result)
     }
 }
@@ -35,6 +52,10 @@ impl Engine {
 fn main() -> Result<()> {
     let file = std::env::args().skip(1).next().expect("No file was passed");
     let mut engine = Engine::new(&file)?;
-    engine.run(&[])?;
+    engine.run(json!({
+        "name": "James",
+        "age": 50,
+        "money": 50.60,
+    }))?;
     Ok(())
 }
